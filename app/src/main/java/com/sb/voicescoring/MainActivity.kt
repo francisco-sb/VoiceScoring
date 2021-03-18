662df35d48c0e1df0ade8e1674bb078907476387
Francisco Sierra <fmsb0193@gmail.com>
2021-03-18 14:00:06 -0600

    Implemented MediaRecorder

--- app/src/main/java/com/sb/voicescoring/MainActivity.kt
+++ app/src/main/java/com/sb/voicescoring/MainActivity.kt
@@ -23,9 +23,9 @@ import java.io.IOException
 class MainActivity : AppCompatActivity() {
 
     //region Recording
-    /*private var output: String? = null
+    private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
-    private var isRecording = false*/
+    private var isRecording = false
     //endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,29 +34,14 @@ class MainActivity : AppCompatActivity() {
 
         requestPermissions()
 
-        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
-
-        val pdh = PitchDetectionHandler { pitchDetectionResult, _ ->
-            val pitchInHz = pitchDetectionResult.pitch
-            runOnUiThread {
-                processPitch(pitchInHz)
-            }
-        }
-
-        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050f, 1024, pdh)
-
-        dispatcher.addAudioProcessor(pitchProcessor)
-
-        val audioThread = Thread(dispatcher, "Audio Thread")
-        audioThread.start()
-        /*@Suppress("DEPRECATION")
+        @Suppress("DEPRECATION")
         output = "${Environment.getExternalStorageDirectory().absolutePath}/recording.mp3"
         mediaRecorder = MediaRecorder()
 
         mediaRecorder?.let {
             it.setAudioSource(MediaRecorder.AudioSource.MIC)
             it.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            it.setAudioEncoder(MediaRecorder.AudioEncoder.AAC_ELD)
+            it.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
             it.setOutputFile(output)
         }
 
@@ -69,13 +54,6 @@ class MainActivity : AppCompatActivity() {
                 btn_microphone.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_microphone, null))
             }
         }
-
-        btn_duplicate.setOnClickListener {
-            output?.let {
-                val file = File(it)
-                file.copyTo(File("${Environment.getExternalStorageDirectory().absolutePath}/recording-copy.mp3"), true)
-            }
-        }*/
     }
 
     //region:: PRIVATE METHODS
@@ -88,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*private fun startRecording() {
+    private fun startRecording() {
         try {
             mediaRecorder?.let {
                 it.prepare()
@@ -112,22 +90,6 @@ class MainActivity : AppCompatActivity() {
             }
         } else {
             Toast.makeText(this, "You are not recording!", Toast.LENGTH_SHORT).show()
-        }
-    }*/
-
-    private fun processPitch(pitchInHz: Float) {
-        val pitch = "Pitch in Hz: $pitchInHz"
-        txt_pitch.text = pitch
-
-        val note = "Note: "
-        when {
-            pitchInHz >= 110 && pitchInHz < 123.47 -> "$note A".also { txt_note.text = it }
-            pitchInHz >= 123.47 && pitchInHz < 130.81 -> "$note B".also { txt_note.text = it }
-            pitchInHz >= 130.81 && pitchInHz < 146.83 -> "$note C".also { txt_note.text = it }
-            pitchInHz >= 146.83 && pitchInHz < 164.81 -> "$note D".also { txt_note.text = it }
-            pitchInHz >= 164.81 && pitchInHz < 174.61 -> "$note E".also { txt_note.text = it }
-            pitchInHz >= 174.61 && pitchInHz < 185 -> "$note F".also { txt_note.text = it }
-            pitchInHz >= 185 && pitchInHz < 196 -> "$note G".also { txt_note.text = it }
         }
     }
     //endregion

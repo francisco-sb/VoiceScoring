92c68e9cd5094a809081abd0e1d5297b999164cf
Francisco Sierra <fmsb0193@gmail.com>
2021-03-22 08:56:09 -0600

    Changed ranks in notes

--- app/src/main/java/com/sb/voicescoring/MainActivity.kt
+++ app/src/main/java/com/sb/voicescoring/MainActivity.kt
@@ -1,32 +1,18 @@
 package com.sb.voicescoring
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.widget.Toast
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import be.tarsos.dsp.AudioDispatcher
 import be.tarsos.dsp.io.android.AudioDispatcherFactory
 import be.tarsos.dsp.pitch.PitchDetectionHandler
 import be.tarsos.dsp.pitch.PitchProcessor
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import java.io.File
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-
-    //region Recording
-    /*private var output: String? = null
-    private var mediaRecorder: MediaRecorder? = null
-    private var isRecording = false*/
-    //endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +22,17 @@ class MainActivity : AppCompatActivity() {
 
         val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
 
-        val pdh = PitchDetectionHandler { pitchDetectionResult, _ ->
+        val pdh = PitchDetectionHandler { pitchDetectionResult, event ->
             val pitchInHz = pitchDetectionResult.pitch
             runOnUiThread {
+                /*if (pitchDetectionResult.isPitched) {
+                    Log.e("PitchDetection", "Pitch: ${pitchDetectionResult.pitch} - Probability: ${pitchDetectionResult.probability}")
+                    Log.e("PitchDetection", "MIDI: ${processMIDI(pitchInHz)}")
+                    Log.e("AudioEvent", "Start Time: ${event.timeStamp} | End Time: ${event.endTimeStamp}")
+                    Log.e("AudioEvent", "Frame length: ${event.frameLength}")
+                }*/
                 processPitch(pitchInHz)
+                Log.e("PitchDetection", "Pitch: $pitchInHz - Is silence: ${event.isSilence(pitchInHz.toDouble())}")
             }
         }
 
@@ -49,33 +42,6 @@ class MainActivity : AppCompatActivity() {
 
         val audioThread = Thread(dispatcher, "Audio Thread")
         audioThread.start()
-        /*@Suppress("DEPRECATION")
-        output = "${Environment.getExternalStorageDirectory().absolutePath}/recording.mp3"
-        mediaRecorder = MediaRecorder()
-
-        mediaRecorder?.let {
-            it.setAudioSource(MediaRecorder.AudioSource.MIC)
-            it.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            it.setAudioEncoder(MediaRecorder.AudioEncoder.AAC_ELD)
-            it.setOutputFile(output)
-        }
-
-        btn_microphone.setOnClickListener {
-            if (!isRecording) {
-                startRecording()
-                btn_microphone.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_stop, null))
-            } else {
-                stopRecording()
-                btn_microphone.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_microphone, null))
-            }
-        }
-
-        btn_duplicate.setOnClickListener {
-            output?.let {
-                val file = File(it)
-                file.copyTo(File("${Environment.getExternalStorageDirectory().absolutePath}/recording-copy.mp3"), true)
-            }
-        }*/
     }
 
     //region:: PRIVATE METHODS
@@ -88,47 +54,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*private fun startRecording() {
-        try {
-            mediaRecorder?.let {
-                it.prepare()
-                it.start()
-                isRecording = true
-                Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: IllegalStateException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun stopRecording() {
-        if (isRecording) {
-            mediaRecorder?.let {
-                it.stop()
-                it.release()
-                isRecording = false
-            }
-        } else {
-            Toast.makeText(this, "You are not recording!", Toast.LENGTH_SHORT).show()
-        }
-    }*/
-
     private fun processPitch(pitchInHz: Float) {
         val pitch = "Pitch in Hz: $pitchInHz"
         txt_pitch.text = pitch
 
-        val note = "Note: "
+        var note = "Note: "
         when {
-            pitchInHz >= 110 && pitchInHz < 123.47 -> "$note A".also { txt_note.text = it }
-            pitchInHz >= 123.47 && pitchInHz < 130.81 -> "$note B".also { txt_note.text = it }
-            pitchInHz >= 130.81 && pitchInHz < 146.83 -> "$note C".also { txt_note.text = it }
-            pitchInHz >= 146.83 && pitchInHz < 164.81 -> "$note D".also { txt_note.text = it }
-            pitchInHz >= 164.81 && pitchInHz < 174.61 -> "$note E".also { txt_note.text = it }
-            pitchInHz >= 174.61 && pitchInHz < 185 -> "$note F".also { txt_note.text = it }
-            pitchInHz >= 185 && pitchInHz < 196 -> "$note G".also { txt_note.text = it }
+            pitchInHz >= 16.35 && pitchInHz < 18.35 -> note += "C0"
+            pitchInHz >= 18.35 && pitchInHz < 20.60 -> note += "D0"
+            pitchInHz >= 20.60 && pitchInHz < 21.83 -> note += "E0"
+            pitchInHz >= 21.83 && pitchInHz < 24.50 -> note += "F0"
+            pitchInHz >= 24.50 && pitchInHz < 27.50 -> note += "G0"
+            pitchInHz >= 27.50 && pitchInHz < 30.87 -> note += "A0"
+            pitchInHz >= 30.87 && pitchInHz < 32.70 -> note += "B0"
+            pitchInHz >= 32.70 && pitchInHz < 36.71 -> note += "C1"
+            pitchInHz >= 36.71 && pitchInHz < 41.20 -> note += "D1"
+            pitchInHz >= 41.20 && pitchInHz < 43.65 -> note += "E1"
+            pitchInHz >= 43.65 && pitchInHz < 49.00 -> note += "F1"
+            pitchInHz >= 49.00 && pitchInHz < 55.00 -> note += "G1"
+            pitchInHz >= 55.00 && pitchInHz < 61.74 -> note += "A1"
+            pitchInHz >= 61.74 && pitchInHz < 65.41 -> note += "B1"
+            pitchInHz >= 65.41 && pitchInHz < 73.42 -> note += "C2"
+            pitchInHz >= 73.42 && pitchInHz < 82.41-> note += "D2"
+            pitchInHz >= 82.41 && pitchInHz < 87.31-> note += "E2"
+            pitchInHz >= 87.31 && pitchInHz < 98.00-> note += "F2"
+            pitchInHz >= 98.00 && pitchInHz < 110.00-> note += "G2"
+            pitchInHz >= 110.00 && pitchInHz < 123.47-> note += "A2"
+            pitchInHz >= 123.47 && pitchInHz < 130.81-> note += "B2"
+            pitchInHz >= 130.81 && pitchInHz < 146.83-> note += "C3"
+            pitchInHz >= 146.83 && pitchInHz < 164.81-> note += "D3"
+            pitchInHz >= 164.81 && pitchInHz < 174.61-> note += "E3"
+            pitchInHz >= 174.61 && pitchInHz < 196.00-> note += "F3"
+            pitchInHz >= 196.00 && pitchInHz < 220.00-> note += "G3"
+            pitchInHz >= 220.00 && pitchInHz < 246.94-> note += "A3"
+            pitchInHz >= 246.94 && pitchInHz < 261.63-> note += "B3"
+            pitchInHz >= 261.63 && pitchInHz < 293.66-> note += "C4"
+            pitchInHz >= 293.66 && pitchInHz < 329.63-> note += "D4"
+            pitchInHz >= 329.63 && pitchInHz < 349.23-> note += "E4"
+            pitchInHz >= 349.23 && pitchInHz < 392.00-> note += "F4"
+            pitchInHz >= 392.00 && pitchInHz < 440.00-> note += "G4"
+            pitchInHz >= 440.00 && pitchInHz < 493.88-> note += "A4"
+            else -> note += "Out of range"
         }
+
+        txt_note.text = note
     }
     //endregion
 }
